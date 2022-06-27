32f7713f042f3f2d88c1d56efe5752996a67b30b
The Machine Ambassador <77938614+TheMachineAmbassador@users.noreply.github.com>
2022-06-27 21:09:47 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "ImGui"
+   kind "StaticLib"
+   language "C++"
+   staticruntime "off"
+
+   targetdir ("%{wks.location}/bin/" .. outputdir)
+   objdir ("%{wks.location}/bin/intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
